e82bf02812532bc40cfded6e569b32244895914c
Felipe Bonato <felipe13.bonato@gmail.com>
2021-05-12 16:50:25 -0300

    Added flash messages

--- App/Controller/Access.php
+++ App/Controller/Access.php
@@ -22,9 +22,13 @@ class Access extends \Core\Controller
 			$_SESSION['user_nickname'] = $user->nickname;
 			$_SESSION['user_email'] = $user->email;
 
+			\App\Flash::add_message('You are now logged in!', \App\Flash::SUCCESS);
+
 			\Core\redirect('');
 			exit;
-		} else {
+		} else { //TODO: WHEN USER CANNOT LOGIN HE IS REDIRECTED TO ACCESS/VALIDE
+			\App\Flash::add_message('Could not login; your email and/or password may be wrong!', \App\Flash::WARNING);
+
 			\Core\View::render_templeate('access/login.html', [
 				'email' => $_POST['email']
 			]);
@@ -33,20 +37,24 @@ class Access extends \Core\Controller
 
 	public function logout_action(): void
 	{
-		if($_SESSION){
-			$_SESSION = array(); // Unset all session variables
-		
-			if(ini_get('session.use_cookies')){ // Delete the cookies
-				$params = session_get_cookie_params();
-				setcookie(session_name(), '', time() - 42000,
-					$params['path'], $params['domain'],
-					$params['secure'], $params['httponly']
-				);
-			}
-
-			session_destroy(); // Delete the session
+		$_SESSION = array(); // Unset all session variables
+	
+		if(ini_get('session.use_cookies')){ // Delete the cookies
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params['path'], $params['domain'],
+				$params['secure'], $params['httponly']
+			);
 		}
 
+		session_destroy(); // Delete the session
+		
+		\Core\redirect('/access/show-logout-message');
+	}
+
+	public function show_logout_message(): void
+	{
+		\App\Flash::add_message('You are logged out!', \App\Flash::SUCCESS);
 		\Core\redirect('');
 	}
 }
