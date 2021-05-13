9129c303d2930c5cf098b53057c5c72ce5c7028a
Felipe Bonato <felipe13.bonato@gmail.com>
2021-05-13 20:10:21 -0300

    Added admin book listing

--- App/Controller/Admin/Book.php
+++ App/Controller/Admin/Book.php
@@ -1,10 +1,14 @@
 <?php namespace App\Controller\Admin;
 
-class User extends \Core\Controller
+class Book extends \Core\Controller
 {
 	public function list_action(): void
 	{
 		$books = \App\Model\Book::get_all();
-		\Core\View::render_templeate('admin/book/list.html', ['books' => $books]);
+		$deleted_books = \App\Model\Book::get_all_deleted();
+		\Core\View::render_templeate('admin/book/list.html', [
+			'books' => $books,
+			'deleted_books' => $deleted_books
+		]);
 	}
 }
