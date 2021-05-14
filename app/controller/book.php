5d1cdf05bc32be37d554f742407e05496c38f35b
Felipe Bonato <felipe13.bonato@gmail.com>
2021-05-13 21:15:05 -0300

    Added book editing

--- app/controller/book.php
+++ app/controller/book.php
@@ -37,15 +37,42 @@ class Book extends \Core\Controller
 		$book = \App\Model\Book::get_book_by_id($id);
 		
 		if(empty($book)){
-			throw new \Exception('Book does not exist');
+			throw new \Exception('Book does not exist', 404);
 			return;
 		}
 
 		\Core\View::render_templeate('book/view.html', [
-			'book' => $book
+			'book' => $book,
+			'user' => \App\Auth::get_user()
 		]);
 	}
 
+	public function edit_action(int $id): void
+	{
+		$book = \App\Model\Book::get_book_by_id($id);
+		
+		\Core\View::render_templeate('book/edit.html', [
+			'book' => $book
+		]);
+		
+		
+		//\App\redirect('book/'.$id.'/success'); // TODO THIS MIGHT NEED TO CHANGE <----------------------------------
+	}
+
+	public function modify_action()
+	{
+		$book = new \App\Model\Book($_POST);
+
+		if($book->modify()){
+			\App\Flash::add_message('Your book was modified successfully!', \App\Flash::SUCCESS);
+			\App\redirect('book/'.$book->id);
+		} else {
+			\App\Flash::add_message('Could not modify your book!', \App\Flash::WARNING);
+			\Core\View::render_templeate('book/modify.html', [
+				'book' => $book
+			]);
+		}
+	}
 
 
 	public function success_action(): void
