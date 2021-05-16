6a9c5079af066106c4d8617f6cb64e200e22bd58
Felipe Bonato <felipe13.bonato@gmail.com>
2021-05-16 18:48:04 -0300

    Small improvements on styling on various things

--- app/controller/home.php
+++ app/controller/home.php
@@ -8,6 +8,8 @@ class Home extends \Core\Controller
 	public function index_action(): void
 	{
 		$books = \App\Model\Book::get_all();
+		shuffle($books);
+
 		\Core\View::render_templeate('home/index.html', [
 			'books' => $books
 		]);
