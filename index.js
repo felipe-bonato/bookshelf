a504cc918e4996678ae5c0b1804dfa7b9d87c717
Felipe Bonato <felipe13.bonato@gmail.com>
2021-07-16 09:28:11 -0300

    Updated to use react

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById('hello').textContent = 'Hello, Wold! (from js)'
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static('public'));
+
+app.get('/', require('./routes').index);
+
+app.listen(8080, () => console.log('Server started!'));
